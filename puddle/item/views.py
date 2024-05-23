af0f36bedf122e8034b822bda757e2a51f841a31
nomthandazo <ndlovunomthandazo4@gmail.com>
2024-05-23 20:44:41 +0200

    Oh

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import NewItemForm
 from .models import Item 
@@ -15,7 +15,21 @@ def detail(request, pk):
 
 @login_required
 def new(request):
-    form = NewItemForm()
+    if(request.method == 'POST'):
+        form = NewItemForm(request.POST, request.FILES)
 
-    return render(request, 'item/form.html')
-    'form': form
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItemForm()
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'New Item',
+    })
+    # <button class="py-4 px-8 text-lg bg-teal-500 hover:bg-teal-700 rounded-xl text-white">Submit</button>
+
